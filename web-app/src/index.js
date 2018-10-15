d13885a9e6b45160a3101fc8879bf6ea6ca1a31e
Edvin Wiberg <edde275@gmail.com>
2018-10-15 21:23:14 +0200

    Created project structure with Wordpress API and React

---
+++ web-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+if(process.env.NODE_ENV === 'production')
+  serviceWorker.register();
+else
+  serviceWorker.unregister();
---
+++ wp-api/public/themes/gustavWhitefield/custom-endpoints/application.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// Handle applications from organisations and individuals
+
+add_action('rest_api_init', function () {
+
+});
