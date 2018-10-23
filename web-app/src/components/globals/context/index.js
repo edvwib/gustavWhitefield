62c1d3afc9ece53e99e302ef96195aeb96ee0655
Edvin Wiberg <edde275@gmail.com>
2018-10-23 13:08:43 +0200

    added fetch for news

--- web-app/src/components/globals/context/index.js
+++ web-app/src/components/globals/context/index.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 
 export const Glob = React.createContext();
 
+
 class ContextProvider extends Component{
 
+  API_URL = `http://localhost:${process.env.PHP_PORT}/wp-json/api/v1/`;
+
   state={
-    eng: false
+    eng: false,
+    news: [],
+  }
+
+  componentDidMount = () => {
+    fetch(this.API_URL + 'news')
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data,
+        })
+      })
   }
 
   render() {
