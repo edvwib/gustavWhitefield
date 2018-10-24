e1583b31fec01be80af3d7e723a083fd554e4fb6
Edvin Wiberg <edde275@gmail.com>
2018-10-24 10:47:33 +0200

    added api route for pages

--- wp-api/public/themes/gustavWhitefield/custom-endpoints/pages.php
+++ wp-api/public/themes/gustavWhitefield/custom-endpoints/pages.php
@@ -3,14 +3,38 @@
 declare (strict_types=1);
 
 add_action('rest_api_init', function () {
+    register_rest_route('api/v1', '/pages/', [
+        'methods' => 'GET',
+        'callback' => 'getAllPages'
+    ]);
     register_rest_route('api/v1', '/page/(?P<slug>.+)', [
         'methods' => 'GET',
         'callback' => 'getPage'
     ]);
 });
 
+function getAllPages(){
+    $pages = get_posts([
+        'post_type' => 'page',
+        'numberposts' => -1,
+    ]);
+    $pagesWithFields = [];
+
+    foreach ($pages as $page) {
+        array_push($pagesWithFields, [
+            'page' => $page,
+            'fields' => get_fields($page->ID),
+        ]);
+    }
+
+    return $pagesWithFields;
+}
+
 function getPage($data)
 {
-    return get_page_by_path($data['slug']);
+    $page = get_page_by_path($data['slug']);
+    $fields = get_fields($page->ID);
+    $page->fields = $fields;
+    return $page;
 }
 
