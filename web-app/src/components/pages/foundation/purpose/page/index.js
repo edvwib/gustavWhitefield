efa553be47d52e5dc750abcb1362f4d676a7e3fe
krisKurken <kristian.liljander@live.se>
2018-10-31 16:53:45 +0100

    add all pages link, made purpose side, and so on

---
+++ web-app/src/components/pages/foundation/purpose/page/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import { Container } from './style';
+
+
+class Page extends Component {
+
+  render() {
+    console.log(this.props.pages);
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
+
+export default Page;
