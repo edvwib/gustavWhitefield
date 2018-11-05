6935c7904d9fb960dd9d6c9fcb194b1cf1edaed7
krisKurken <kristian.liljander@live.se>
2018-11-05 15:37:09 +0100

    init we support page

--- web-app/src/components/pages/foundation/weSupport/index.js
+++ web-app/src/components/pages/foundation/weSupport/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import { Glob } from '../../../../components/globals/context';
 import { Container } from './style';
 
+import Page from './page';
 
 class WeSupport extends Component {
 
   render() {
     return (
-      <Container>
-        <h1>About WeSupport</h1>
-      </Container>
+      <Glob.Consumer>
+        {
+          (context) => (
+            <React.Fragment>
+              <Page eng={context.state.eng} page={context.state.pages.weSupport}/>
+              {console.log(context.state.pages.weSupport)}
+            </React.Fragment>
+          )
+        }
+      </Glob.Consumer>
     );
   }
 }
---
+++ web-app/src/components/pages/foundation/weSupport/page/content/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Image } from './style';
+
+import Member from './member';
+
+const Content = (props) => {
+
+  return (
+    <Container>
+
+    </Container>
+  );
+}
+
+export default Content;
