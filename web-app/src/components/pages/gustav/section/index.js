8a5815989ef5e6ceabfd634cedccac77eead1850
krisKurken <kristian.liljander@live.se>
2018-11-15 17:11:01 +0100

    search way change

--- web-app/src/components/pages/gustav/section/index.js
+++ web-app/src/components/pages/gustav/section/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Container, Image } from './style';
+import placeholder from '../../../../resources/icons/placeholder.jpg';
 
 const Section = (props) => {
 
   return (
     <Container>
-      <Image src={props.img ? props.img.sizes.large : '/resources/icons/placeholder.jpg'}/>
+      <Image src={props.img ? props.img.sizes.large : placeholder}/>
       <span dangerouslySetInnerHTML={{__html: props.content}}/>
     </Container>
   );
