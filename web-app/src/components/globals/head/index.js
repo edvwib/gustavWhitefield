7b457e741ced0636d8fbabdf390728c23357083e
krisKurken <kristian.liljander@live.se>
2018-11-16 13:07:15 +0100

    meta work

--- web-app/src/components/globals/head/index.js
+++ web-app/src/components/globals/head/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 const Head = (props) => {
   return(
-    <Helmet>
-      <meta charset="utf-8"/>
+    <Helmet >
       <title>{`Gustav Whitefield - ${props.title}`}</title>
     </Helmet>
   )
