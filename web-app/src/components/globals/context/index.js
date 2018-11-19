8496c10291ad22a0671bdfe6907d73a57b5dd1e3
Edvin Wiberg <edde275@gmail.com>
2018-11-19 11:18:05 +0100

    save pages and news to localstorage

--- web-app/src/components/globals/context/index.js
+++ web-app/src/components/globals/context/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export const Glob = React.createContext();
 
-class ContextProvider extends Component{
+class ContextProvider extends Component {
 
   API_URL = process.env.NODE_ENV === 'production' ?
     'https://gustavwhitefield.com/wp-json/api/v1/' :
@@ -16,25 +16,37 @@ class ContextProvider extends Component{
   }
 
   componentDidMount = () => {
-    this.setState({cookieConsent: this.cookieConsent()})
-    this.getData('news');
-    this.getData('pages');
+    let cookieConsent = this.cookieConsent();
+    this.setState({ cookieConsent });
+    this.getData('news', cookieConsent);
+    this.getData('pages', cookieConsent);
   }
 
-  getData = (endpoint) => {
+  getData = (endpoint, cookieConsent) => {
+    if (cookieConsent) {
+      let lastFetch = parseInt(localStorage.getItem(`${endpoint}Fetched`));
+      let data = localStorage.getItem(endpoint);
+
+      // if data is saved and 12 hours have not passed
+      if (data && lastFetch > Date.now() - 43200000) {
+        this.setState({ [endpoint]: JSON.parse(data) });
+        return;
+      }
+    }
+
     fetch(this.API_URL + endpoint)
-    .then((data) => {
-      return data.json();
-    })
-    .then((data) => {
-      this.setState({
-        [endpoint]: data,
+      .then((blob) => blob.json())
+      .then((data) => {
+        this.setState({ [endpoint]: data });
+        if (cookieConsent) {
+          this.saveToLocalStorage(endpoint, JSON.stringify(data));
+          this.saveToLocalStorage(`${endpoint}Fetched`, Date.now());
+        }
       })
-    })
   }
 
   updateLang = () => {
-    if(this.state.cookieConsent)
+    if (this.state.cookieConsent)
       window.localStorage.setItem('eng', !this.state.eng ? 'true' : '');
     this.setState({
       eng: !this.state.eng
@@ -47,6 +59,12 @@ class ContextProvider extends Component{
 
   cookieConsent = () => {
     return document.cookie.split(';').filter((item) => item.includes('cookieconsent_status=allow')).length ? true : false;
+  }
+
+  saveToLocalStorage = (key, data) => {
+    if (this.state.cookieConsent) {
+      window.localStorage.setItem(key, data);
+    }
   }
 
   render() {
